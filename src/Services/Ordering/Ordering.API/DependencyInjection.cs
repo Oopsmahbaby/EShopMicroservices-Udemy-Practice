e62bcb270a10c705f5b8dbdad1c7d221e61366db
Thanh_An <vuthanhan3547@gmail.com>
2025-08-24 21:40:19 +0700

    [Add] Done Chap 14

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿namespace Ordering.API
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddApiServices(this IServiceCollection services)
+		{
+			// Register API services here
+			// Example: services.AddControllers();
+			// Example: services.AddSwaggerGen();
+			return services;
+		}
+
+		public static WebApplication UseApiServices(this WebApplication app)
+		{
+			// Configure API middleware here
+			// Example: app.UseSwagger();
+			// Example: app.UseAuthorization();
+			app.MapControllers();
+			return app;
+		}
+	}
+}
