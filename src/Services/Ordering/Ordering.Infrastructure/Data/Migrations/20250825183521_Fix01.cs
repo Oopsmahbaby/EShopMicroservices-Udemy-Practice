ed0c30f1167465bb5b63b3e05374fa30f3097a97
Thanh_An <vuthanhan3547@gmail.com>
2025-08-28 22:48:20 +0700

    [Update] Refactor

--- src/Services/Ordering/Ordering.Infrastructure/Data/Migrations/20250825183521_Fix01.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Migrations/20250825183521_Fix01.cs
@@ -4,95 +4,95 @@
 
 namespace Ordering.Infrastructure.Data.Migrations
 {
-    /// <inheritdoc />
-    public partial class Fix01 : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.RenameColumn(
-                name: "LastModifiedDate",
-                table: "Products",
-                newName: "LastModified");
+	/// <inheritdoc />
+	public partial class Fix01 : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.RenameColumn(
+				name: "LastModifiedDate",
+				table: "Products",
+				newName: "LastModified");
 
-            migrationBuilder.RenameColumn(
-                name: "CreatedDate",
-                table: "Products",
-                newName: "CreatedAt");
+			migrationBuilder.RenameColumn(
+				name: "CreatedDate",
+				table: "Products",
+				newName: "CreatedAt");
 
-            migrationBuilder.RenameColumn(
-                name: "LastModifiedDate",
-                table: "Orders",
-                newName: "LastModified");
+			migrationBuilder.RenameColumn(
+				name: "LastModifiedDate",
+				table: "Orders",
+				newName: "LastModified");
 
-            migrationBuilder.RenameColumn(
-                name: "CreatedDate",
-                table: "Orders",
-                newName: "CreatedAt");
+			migrationBuilder.RenameColumn(
+				name: "CreatedDate",
+				table: "Orders",
+				newName: "CreatedAt");
 
-            migrationBuilder.RenameColumn(
-                name: "LastModifiedDate",
-                table: "OrderItems",
-                newName: "LastModified");
+			migrationBuilder.RenameColumn(
+				name: "LastModifiedDate",
+				table: "OrderItems",
+				newName: "LastModified");
 
-            migrationBuilder.RenameColumn(
-                name: "CreatedDate",
-                table: "OrderItems",
-                newName: "CreatedAt");
+			migrationBuilder.RenameColumn(
+				name: "CreatedDate",
+				table: "OrderItems",
+				newName: "CreatedAt");
 
-            migrationBuilder.RenameColumn(
-                name: "LastModifiedDate",
-                table: "Customers",
-                newName: "LastModified");
+			migrationBuilder.RenameColumn(
+				name: "LastModifiedDate",
+				table: "Customers",
+				newName: "LastModified");
 
-            migrationBuilder.RenameColumn(
-                name: "CreatedDate",
-                table: "Customers",
-                newName: "CreatedAt");
-        }
+			migrationBuilder.RenameColumn(
+				name: "CreatedDate",
+				table: "Customers",
+				newName: "CreatedAt");
+		}
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.RenameColumn(
-                name: "LastModified",
-                table: "Products",
-                newName: "LastModifiedDate");
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.RenameColumn(
+				name: "LastModified",
+				table: "Products",
+				newName: "LastModifiedDate");
 
-            migrationBuilder.RenameColumn(
-                name: "CreatedAt",
-                table: "Products",
-                newName: "CreatedDate");
+			migrationBuilder.RenameColumn(
+				name: "CreatedAt",
+				table: "Products",
+				newName: "CreatedDate");
 
-            migrationBuilder.RenameColumn(
-                name: "LastModified",
-                table: "Orders",
-                newName: "LastModifiedDate");
+			migrationBuilder.RenameColumn(
+				name: "LastModified",
+				table: "Orders",
+				newName: "LastModifiedDate");
 
-            migrationBuilder.RenameColumn(
-                name: "CreatedAt",
-                table: "Orders",
-                newName: "CreatedDate");
+			migrationBuilder.RenameColumn(
+				name: "CreatedAt",
+				table: "Orders",
+				newName: "CreatedDate");
 
-            migrationBuilder.RenameColumn(
-                name: "LastModified",
-                table: "OrderItems",
-                newName: "LastModifiedDate");
+			migrationBuilder.RenameColumn(
+				name: "LastModified",
+				table: "OrderItems",
+				newName: "LastModifiedDate");
 
-            migrationBuilder.RenameColumn(
-                name: "CreatedAt",
-                table: "OrderItems",
-                newName: "CreatedDate");
+			migrationBuilder.RenameColumn(
+				name: "CreatedAt",
+				table: "OrderItems",
+				newName: "CreatedDate");
 
-            migrationBuilder.RenameColumn(
-                name: "LastModified",
-                table: "Customers",
-                newName: "LastModifiedDate");
+			migrationBuilder.RenameColumn(
+				name: "LastModified",
+				table: "Customers",
+				newName: "LastModifiedDate");
 
-            migrationBuilder.RenameColumn(
-                name: "CreatedAt",
-                table: "Customers",
-                newName: "CreatedDate");
-        }
-    }
+			migrationBuilder.RenameColumn(
+				name: "CreatedAt",
+				table: "Customers",
+				newName: "CreatedDate");
+		}
+	}
 }
