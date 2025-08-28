639ef7e34e1086f9e7e544c7fe0828e78055308f
Thanh_An <vuthanhan3547@gmail.com>
2025-08-28 23:53:45 +0700

    [Add] Done vid 237 chap 16

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Exceptions;
+
+namespace Ordering.Application.Exceptions
+{
+	public class OrderNotFoundException : NotFoundException
+	{
+
+		public OrderNotFoundException(Guid id) : base($"Order with id '{id}' was not found.")
+		{
+		}
+	}
+}
