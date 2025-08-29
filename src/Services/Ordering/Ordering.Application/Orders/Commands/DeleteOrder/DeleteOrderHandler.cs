4bde27d0ccfce026414591b0fb6a7f940453fb2b
Thanh_An <vuthanhan3547@gmail.com>
2025-08-29 20:54:08 +0700

    [Add] Done chap 16

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+	public class DeleteOrderHandler(IApplicationDbContext dbContext) : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+	{
+		public async Task<DeleteOrderResult> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+		{
+			// Implementation logic to delete an order would go here.
+			// Delete the order with the given OrderId from the database.
+			// Return the result indicating success or failure.
+
+			var orderId = OrderId.Of(request.OrderId);
+			var order = await dbContext.Orders.FindAsync([orderId], cancellationToken);
+			if (order == null)
+			{
+				throw new OrderNotFoundException(request.OrderId);
+			}
+
+			dbContext.Orders.Remove(order);
+			await dbContext.SaveChangesAsync(cancellationToken);
+			return new DeleteOrderResult(true);
+		}
+	}
+}
